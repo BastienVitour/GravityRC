18a0b9cbcc379c3d8ca63dbf29275fc6d11336ae
Jean Fourest <jmfourest@gmail.com>
2024-11-14 10:56:07 +0100

    fix checkpoints + new button

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class Checkpoint : MonoBehaviour
+public class Button : MonoBehaviour
 {
-    Vector2 pointupdate;
+    public GameObject door;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -21,9 +21,7 @@ public class Checkpoint : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") // filter the objects that collide with the checkpoint. You can assign the tag in the inspector
         {
-            pointupdate = new Vector2( transform.position.x,transform.position.y);
-            Debug.Log($"{pointupdate.x}, {pointupdate.y}");
-            other.GetComponent<CharacterScript>().spawn = pointupdate;
+            Destroy(door);
         }
     }
 }
